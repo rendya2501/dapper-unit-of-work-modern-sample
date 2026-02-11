9b67d80a5af809882d1c122d2ef87bf45842af5e
伊藤 駿 <rendya2501@gmail.com>
2026-02-11 22:55:35 +0900

    InventoryドメインモデルをInventoryResponseに変換する拡張メソッドにドキュメントコメントを追加しました。

--- src/Web.Api/Contracts/Inventory/Responses/InventoryMappingExtensions.cs
+++ src/Web.Api/Contracts/Inventory/Responses/InventoryMappingExtensions.cs
@@ -1,7 +1,15 @@
 ﻿namespace Web.Api.Contracts.Inventory.Responses;
 
+/// <summary>
+/// <see cref="Inventory"/> ドメインモデルを <see cref="InventoryResponse"/> に変換する拡張メソッド
+/// </summary>
 public static class InventoryMappingExtensions
 {
+    /// <summary>
+    /// <see cref="Inventory"/> を <see cref="InventoryResponse"/> に変換します
+    /// </summary>
+    /// <param name="inventory">変換対象の在庫エンティティ</param>
+    /// <returns>在庫情報のレスポンスDTO</returns>
     public static InventoryResponse ToResponse(this Domain.Inventory.Inventory inventory) =>
         new(
             inventory.ProductId,
